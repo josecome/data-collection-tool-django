e5b16b590e12ff48c5e2171db59bc7186bd3889c
Jose Jaime Come <josejaimecome@gmail.com>
2023-09-10 19:03:24 +0200

    Middleware added

--- data_collection_tool_django/settings.py
+++ data_collection_tool_django/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'forms.middleware.RoleMiddleware',
 ]
 
 ROOT_URLCONF = 'data_collection_tool_django.urls'
