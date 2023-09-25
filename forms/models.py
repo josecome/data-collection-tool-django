fa0a269941aa70bceac2a42b73c8395665b4076e
Jose Jaime Come <josejaimecome@gmail.com>
2023-09-25 18:49:18 +0200

    Minor changes made

--- forms/models.py
+++ forms/models.py
@@ -2,15 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
+
 # Create your models here.
-class Project_Form_Meta(models.Model):
+class SharedFields(models.Model):
+    created_date = models.DateField(null=True)
+    updated_date = models.DateField(null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Project_Form_Meta(SharedFields):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     form_name = models.CharField(max_length=80)
     form_description = models.CharField(max_length=160)
     form_country = models.CharField(max_length=40)
     form_field = models.CharField(max_length=80) # Where the form will be used
-    created_date = models.DateField(null=True)
-    updated_date = models.DateField(null=True)
     user = models.ForeignKey(User,        
                             models.SET_NULL,
                             blank=True,
@@ -30,33 +37,27 @@ class Project_Form_Meta(models.Model):
         super().save(*args, **kwargs)
 
 
-class Project_Form(models.Model):
+class Project_Form(SharedFields):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     field_name = models.CharField(max_length=80)
     field_label = models.CharField(max_length=80, default='Label')
     field_description = models.CharField(max_length=160)
     field_type = models.CharField(max_length=160)
     field_size = models.IntegerField()
-    created_date = models.DateField()
-    updated_date = models.DateField()  
     form_meta = models.ForeignKey(Project_Form_Meta,  models.SET_NULL, blank=True, null=True) 
     user = models.ForeignKey(User,  models.SET_NULL, blank=True, null=True)
 
 
-class Form_Sql_Query_Db(models.Model):
+class Form_Sql_Query_Db(SharedFields):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     sql_query = models.TextField()
-    created_date = models.DateField()
-    updated_date = models.DateField()  
     form_meta = models.ForeignKey(Project_Form_Meta,  models.SET_NULL, blank=True, null=True) 
     user = models.ForeignKey(User,  models.SET_NULL, blank=True, null=True)
 
 
-class FieldwithChoiceOptions(models.Model):
+class FieldwithChoiceOptions(SharedFields):
     id = models.IntegerField(primary_key=True, default=uuid.uuid4, editable=False)
     option_name = models.CharField(max_length=160)
     option_label = models.CharField(max_length=160)
-    created_date = models.DateField()
-    updated_date = models.DateField()  
     project_form = models.ForeignKey(Project_Form,  models.SET_NULL, blank=True, null=True) 
     user = models.ForeignKey(User,  models.SET_NULL, blank=True, null=True)
